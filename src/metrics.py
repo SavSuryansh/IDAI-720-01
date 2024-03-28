9796fbf60f73d04691ef70ec07d55c14d20a208a
SavSuryansh <ss3964@rit.edu>
2024-03-28 12:35:52 -0400

    Filled metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -16,7 +16,10 @@ class Metrics:
         # return: EOD = TPR(s=1)-TPR(s=0)
         # TPR = #(y=1, y_pred=1) / #(y=1)
         # Write your code below:
-
+        sensitive = np.array(s_attr)
+        condition_positive = self.actual == 1
+        tpr1 = np.mean(self.predictions[condition_positive & (sensitive == 1)] == 1)
+        tpr0 = np.mean(self.predictions[condition_positive & (sensitive == 0)] == 1)
         return tpr1-tpr0
 
     def aod(self, s):
@@ -26,7 +29,13 @@ class Metrics:
         # TPR = #(y=1, y_pred=1) / #(y=1)
         # FPR = #(y=0, y_pred=1) / #(y=0)
         # Write your code below:
-
+        sensitive = np.array(s_attr)
+        true_pos = self.actual == 1
+        false_pos = self.actual == 0
+        tpr1 = np.mean(self.predictions[true_pos & (sensitive == 1)] == 1)
+        tpr0 = np.mean(self.predictions[true_pos & (sensitive == 0)] == 1)
+        fpr1 = np.mean(self.predictions[false_pos & (sensitive == 1)] == 1)
+        fpr0 = np.mean(self.predictions[false_pos & (sensitive == 0)] == 1)
         return (tpr1 - tpr0 + fpr1 - fpr0)/2.0
 
     def spd(self, s):
@@ -35,7 +44,8 @@ class Metrics:
         # return: SPD = |PR(s=1)-PR(s=0)|
         # PR = #(y_pred=1) / #y
         # Write your code below:
-
+        sensitive = np.array(s_attr)
+        pr1 = np.mean(self.predictions[sensitive == 1] == 1)
+        pr0 = np.mean(self.predictions[sensitive == 0] == 1)
         return pr1 - pr0
-
 
